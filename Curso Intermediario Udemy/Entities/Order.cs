df2e4685ffe812498793b797f7f0975f96617b42
Isac R Horta <isacbhz@hotmail.com>
2020-04-06 20:09:59 -0300

    Aula 115 Enumeradores

---
+++ Curso Intermediario Udemy/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Curso_Intermediario_Udemy.Entities.Enums;
+
+namespace Curso_Intermediario_Udemy.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + "," + Moment + "," + Status;
+        }
+    }
+}
--- Curso Intermediario Udemy/Program.cs
+++ Curso Intermediario Udemy/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Curso_Intermediario_Udemy.Entities;
+using System;
+using Curso_Intermediario_Udemy.Entities.Enums;
 
 namespace Curso_Intermediario_Udemy
 {
@@ -6,7 +8,20 @@ namespace Curso_Intermediario_Udemy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+             Console.WriteLine(txt);
+            Console.WriteLine(os);
+            Console.ReadLine();
         }
     }
 }
