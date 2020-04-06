e2c7f4943150e3ed8fe2a2fa9f90f2232fafb435
Isac R Horta <isacbhz@hotmail.com>
2020-04-06 19:53:04 -0300

    Versao Inicial

---
+++ Curso Intermediario Udemy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso_Intermediario_Udemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
