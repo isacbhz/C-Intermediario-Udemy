e97c00e99e30e6e0a9b45905d5475fda05afea14
Isac R Horta <isacbhz@hotmail.com>
2020-04-08 19:38:01 -0300

    Aula 119-Composicao

---
+++ Aula 119 _Composicao_WorkerContract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_119__Composicao_WorkerContract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
