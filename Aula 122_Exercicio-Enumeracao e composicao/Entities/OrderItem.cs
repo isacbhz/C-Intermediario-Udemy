a8a73cde8f8b1ddc793123535778391dc154c3fa
isacbhz2 <isacbhz@hotmail.com>
2020-04-14 11:43:58 -0300

    Aula 122 - implementacao metodo subTotal Orderitem

--- Aula 122_Exercicio-Enumeracao e composicao/Entities/OrderItem.cs
+++ Aula 122_Exercicio-Enumeracao e composicao/Entities/OrderItem.cs
@@ -8,6 +8,15 @@ namespace Aula_122_Exercicio_Enumeracao_e_composicao.Entities
     {
         public int Quantity { get; set; }
         public double Price { get; set; }
-        public int MyProperty { get; set; }
+        public OrderItem() { }
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+        public double subTotal()
+        {
+            return Quantity * Price;
+        }
     }
 }
