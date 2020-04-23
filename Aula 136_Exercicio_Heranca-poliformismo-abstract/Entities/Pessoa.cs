11f1646a10ef192cc92e86ffe18c5c56e28e6149
Isac R Horta <isacbhz@hotmail.com>
2020-04-23 20:00:01 -0300

    Criacao Classes

---
+++ Aula 136_Exercicio_Heranca-poliformismo-abstract/Entities/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula_136_Exercicio_Heranca_poliformismo_abstract.Entities
+{
+    class Pessoa
+    {
+    }
+}
---
+++ Aula 136_Exercicio_Heranca-poliformismo-abstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_136_Exercicio_Heranca_poliformismo_abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
