0c7a7172c546befad8ce7ea826198462929b723a
Isac R Horta <isacbhz@hotmail.com>
2020-04-23 20:42:26 -0300

    Implementacao Metodos classe Pessoa Fisica

--- Aula 136_Exercicio_Heranca-poliformismo-abstract/Entities/PessoaFisica.cs
+++ Aula 136_Exercicio_Heranca-poliformismo-abstract/Entities/PessoaFisica.cs
@@ -1,16 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace Aula_136_Exercicio_Heranca_poliformismo_abstract.Entities
 {
     class PessoaFisica : Pessoa
     {
-        public double TaxePercentual { get; set; }
+        public double PercentualTaxe { get; set; }
         public double HealthExpenditures { get; set; }
+        public PessoaFisica(double percentualTaxe, double healthExpenditures, string name, double anualIncome) : base(name,anualIncome)
+        {
+            PercentualTaxe = percentualTaxe;
+            HealthExpenditures = healthExpenditures;
+        }
         public override double TaxesCalculation()
         {
-            throw new NotImplementedException();
+            double TotalTaxe = AnualIncome;
+            if (AnualIncome < 20000.00)
+            {
+                Console.Write("Health expedintures: ");
+                double healthExp = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (healthExp > 0.00)
+                {                    
+                    TotalTaxe *= 0.25 + (HealthTaxes(healthExp));
+                }
+                else
+                {
+                    TotalTaxe *= 0.15;
+                }
+            }
+            return TotalTaxe;
+        }
+        public double HealthTaxes(double healthExp)
+        {
+            double calc = 0.00;
+            calc = (healthExp * 0.25);
+            return calc;
         }
     }
 }
