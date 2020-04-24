648a55c25249ce36f1c403529df6e37f457d4c95
Isac R Horta <isacbhz@hotmail.com>
2020-04-23 21:18:23 -0300

    Implementacao Classe Enum

---
+++ Aula 136_Exercicio_Heranca-poliformismo-abstract/Entities/Enums/TypeApplicationTaxe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula_136_Exercicio_Heranca_poliformismo_abstract.Entities.Enums
+{
+    enum TypeApplicationTaxe:int
+    {
+        Individual =0,
+        Company =1
+    }
+}
